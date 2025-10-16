43a4995be3269bb5eeeff392fc9147e42d671fc4
Thomas Shaw <thomas.shaw@st-annes.ox.ac.uk>
2025-10-16 11:25:04 +0100

    Add rainfall processing placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -27,3 +27,6 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
+#TODO(tshaw-ox): Add call to process rainfall
+
